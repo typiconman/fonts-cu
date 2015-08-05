e161405b95c507ae64d87271898a06ae2249c9d2
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2015-08-06 02:27:46 +0300

    Adding Graphite features and documentation for Pomorsky

---
+++ Pomorsky/genfonts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+PACK_NAME=PomorskyUnicode
+DOCS="README LICENSE OFL.txt PomorskyUnicode.pdf CHANGES"
+VERSION="0.5"
+
+## delete binaries
+rm PomorskyUnicode.otf
+rm PomorskyUnicode.ttf
+rm PomorskyUnicode.woff
+
+## get FontForge to generate all necessary files
+fontforge -script hp-generate.py
+wait
+
+## generate Graphite version
+grcompiler -wall PomorskyUnicode.gdl PomorskyUnicode.ttf PomorskyUnicodeSIL.ttf
+wait
+
+## install the fonts on the system
+mv PomorskyUnicodeSIL.ttf PomorskyUnicode.ttf
+cp PomorskyUnicode.otf ~/.fonts/
+cp PomorskyUnicode.ttf ~/.fonts/
+
+## run the perl script first
+rm PomorskyUnicode.pdf
+#perl ../codechart.pl PomorskyUnicode.otf
+#wait
+
+## compile the demo file
+xelatex PomorskyUnicode.tex
+wait
+
+## create the ZIP archives
+zip -DrX PomorskyUnicode-$VERSION.zip PomorskyUnicode.otf PomorskyUnicode.ttf PomorskyUnicode.woff $DOCS
+zip -DrX PomorskyUnicode-src.zip PomorskyUnicode.sfd PomorskyUnicode.gdl PomorskyUnicode-WOFF-metadata.xml $DOCS
+
---
+++ Pomorsky/hp-generate.py
@@ -0,0 +1,56 @@
+import fontforge;
+
+##
+## THIS FILE IS A FONTFORGE SCRIPT THAT GENERATES THE POMORSKY FONT FAMILY
+##
+##
+
+base_name = "PomorskyUnicode"
+full_name  = "Pomorsky Unicode"
+
+
+#fontforge.setPrefs ("AutoHint", False)
+#fontforge.setPrefs ("ClearInstrsBigChanges",False )
+#fontforge.setPrefs ( "CopyTTFInstrs",False )
+
+## open up the font
+font = fontforge.open(base_name + ".sfd")
+
+## Evidently, this can break Evince, so it may need to be commented out
+font.head_optimized_for_cleartype = True
+
+## not sure if we need this
+#font.encoding = "mac"
+
+ttnames = list( font.sfnt_names )
+for ttname in ttnames:
+	if ttname[1] == 'SubFamil':
+		ttnames.append( ( ttname[0],'Fullname',"%s %s" % ( full_name,ttname[2] ) ) )
+font.sfnt_names = tuple( ttnames )
+
+font.generate( base_name + ".otf", flags=( "opentype", "PfEd-colors", "PfEd-lookups"), layer="Fore" )
+
+woff_meta = base_name + "-WOFF-metadata.xml"
+f = file( woff_meta, 'r')
+lines = f.readlines()
+f.close()
+font.woffMetadata = "".join( lines )
+font.generate( base_name + ".woff", flags=( "opentype"), layer="TTFLayer" )
+
+# Append the TT suffix
+for i in range( 0, len( ttnames )):
+	ttname = ttnames[i]
+	if ttname[1] == 'Fullname':
+		ttnames[i] = ( ttname[0],'Fullname',ttname[2].replace( full_name,full_name + " TT" ) )
+	if ttname[1] == 'Preferred Family':
+		ttnames[i] = ( ttname[0],'Preferred Family',ttname[2].replace( full_name,full_name + " TT" ) )
+
+font.sfnt_names = tuple( ttnames )
+font.familyname = full_name + " TT"
+font.fullname = full_name + " TT"
+font.fontname = base_name + "TT"
+
+font.generate( base_name + ".ttf", flags=( "opentype", "old-kern", "PfEd-colors", "PfEd-lookups", "dummy-dsig" ), layer="TTFLayer" )
+font.close()
+
+
